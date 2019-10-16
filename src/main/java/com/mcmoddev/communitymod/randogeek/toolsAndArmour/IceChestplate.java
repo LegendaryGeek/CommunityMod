e71696236af58aef477b310fe3127db328b4fe2e
LegendaryGeek <timothydaley4@gmail.com>
2019-10-16 14:39:14 -0400

    copied over classes from Geek's Random Stuff

---
+++ src/main/java/com/mcmoddev/communitymod/randogeek/toolsAndArmour/IceChestplate.java
@@ -0,0 +1,16 @@
+package com.mcmoddev.communitymod.randogeek.toolsAndArmour;
+
+import com.mcmoddev.communitymod.randogeek.RandoGeek;
+
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class IceChestplate extends ItemArmor  {
+
+	public IceChestplate() {
+		super(RandoGeek.ICE_ARMOR_MATERIAL, 3, EntityEquipmentSlot.CHEST);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
